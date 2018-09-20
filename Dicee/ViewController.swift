713aa13193017bcf1b2c9623798600b5f25c65d6
Your Name <you@example.com>
2018-09-20 14:43:01 -0600

    new commit

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         updateDiceImages()
         
     }
@@ -43,6 +43,12 @@ class ViewController: UIViewController {
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
         
     }
     
